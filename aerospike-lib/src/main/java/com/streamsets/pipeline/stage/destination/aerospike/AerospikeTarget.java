ca2cb4e35cd677c71710c69afb24f8aac4b3dad6
jboone100 <jboone@aerospike.com>
2018-01-09 15:53:36 -0800

    Use async java client for writes.

---
+++ aerospike-lib/src/main/java/com/streamsets/pipeline/stage/destination/aerospike/AerospikeTarget.java
@@ -0,0 +1,330 @@
+/**
+ * Copyright 2015 StreamSets Inc.
+ *
+ * Licensed under the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.streamsets.pipeline.stage.destination.aerospike;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.aerospike.client.AerospikeClient;
+import com.aerospike.client.async.AsyncClient;
+import com.aerospike.client.AerospikeException;
+import com.aerospike.client.Bin;
+import com.aerospike.client.Key;
+import com.aerospike.client.Value;
+import com.aerospike.client.policy.ClientPolicy;
+import com.aerospike.client.async.AsyncClientPolicy;
+import com.aerospike.client.policy.WritePolicy;
+import com.streamsets.pipeline.api.Batch;
+import com.streamsets.pipeline.api.Field;
+import com.streamsets.pipeline.api.Record;
+import com.streamsets.pipeline.api.StageException;
+import com.streamsets.pipeline.api.base.BaseTarget;
+import com.streamsets.pipeline.api.base.OnRecordErrorException;
+import com.streamsets.pipeline.api.impl.Utils;
+import com.streamsets.pipeline.stage.destination.display.BinNameTooLongAction;
+import com.streamsets.pipeline.stage.destination.display.DataType;
+import com.streamsets.pipeline.stage.destination.display.Groups;
+
+/**
+ * This target is an example and does not actually write to any destination.
+ */
+public class AerospikeTarget extends BaseTarget {
+
+	private static final int MAX_BIN_NAME = 14;
+	private static final Logger LOG = LoggerFactory.getLogger(AerospikeTarget.class);
+	private AsyncClient client = null;
+	private AerospikeTargetConfig conf = null;
+	public AerospikeTarget(AerospikeTargetConfig conf) {
+		this.conf = conf;
+
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	protected List<ConfigIssue> init() {
+		// Validate configuration values and open any required resources.
+		List<ConfigIssue> issues = super.init();
+
+		if (conf.seedHost.isEmpty()) {
+			issues.add(
+					getContext().createConfigIssue(
+							Groups.DATABASE.name(), "config", Errors.INVALID_CONFIGURATION, "Missing Aerospike host"
+							)
+					);
+		}
+
+		if (conf.port <= 1024 || conf.port > 65535) {
+			issues.add(
+					getContext().createConfigIssue(
+							Groups.DATABASE.name(), "config", Errors.INVALID_CONFIGURATION, "Invalid Aerospike port"
+							)
+					);
+		}
+
+		if (conf.connectionTimeout < 0) {
+			issues.add(
+					getContext().createConfigIssue(
+							Groups.DATABASE.name(), "config", Errors.INVALID_CONFIGURATION, "Invalid connection timeout"
+							)
+					);
+		}
+
+		if (conf.namespaceName.isEmpty()) {
+			issues.add(
+					getContext().createConfigIssue(
+							Groups.FIELD_MAPPING.name(), "config", Errors.INVALID_CONFIGURATION, "Missing namespace name"
+							)
+					);
+		}
+
+		if (issues.size() == 0) {
+			// Try to connect to the cluster
+			AsyncClientPolicy policy = new AsyncClientPolicy();
+			if(conf.authProviderOption != AuthProviderOption.NONE){
+				policy.user = conf.username;
+				policy.password = conf.password;
+			}
+			
+			policy.failIfNotConnected = true;
+			policy.timeout = conf.connectionTimeout;
+			try {
+				client = new AsyncClient(policy, conf.seedHost, conf.port);
+				if (conf != null) {
+					if (conf.aerospikeFieldMapping.isEmpty()) {
+						issues.add(
+								getContext().createConfigIssue(
+										"DATABASE",
+										"conf.aerospikeFieldMapping",
+										Errors.INVALID_FIELD_MAPPING,
+										conf.aerospikeFieldMapping,
+										Errors.INVALID_FIELD_MAPPING.getMessage()
+										)
+								);
+
+					}
+				}
+			}
+			catch (AerospikeException ae) {
+				issues.add(getContext().createConfigIssue("DATABASE", "Seed Host", Errors.COULD_NOT_CONNECT, 
+						conf.seedHost, conf.port, ae.toString()));
+			}
+		}
+		// If issues is not empty, the UI will inform the user of each configuration issue in the list.
+		return issues;
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public void destroy() {
+		// Clean up any open resources.
+		super.destroy();
+		if (client != null) {
+			client.close();
+		}
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public void write(Batch batch) throws StageException {
+		Iterator<Record> batchIterator = batch.getRecords();
+
+		while (batchIterator.hasNext()) {
+			Record record = batchIterator.next();
+			try {
+				write(record);
+			} catch (Exception e) {
+				switch (getContext().getOnErrorRecord()) {
+				case DISCARD:
+					break;
+				case TO_ERROR:
+					getContext().toError(record, Errors.SAMPLE_01, e.toString());
+					break;
+				case STOP_PIPELINE:
+					throw new StageException(Errors.SAMPLE_01, e.toString());
+				default:
+					throw new IllegalStateException(
+							Utils.format("Unknown OnError value '{}'", getContext().getOnErrorRecord(), e)
+							);
+				}
+			}
+		}
+	}
+
+	private int getTimeToLive(String ttl, Record record) {
+		try {
+			return Integer.parseInt(ttl);
+		}
+		catch (NumberFormatException nfe) {
+			Field field = record.get(ttl);
+			if (field != null) {
+				try {
+					return field.getValueAsInteger();
+				}
+				catch (Exception e) {
+					LOG.error("Field {} has a value of {}, which is not a valid integer", ttl, field.getValue());
+				}
+			}
+		}
+		LOG.error("Could not find a valid TTL from configuration value '{}', assuming 0", ttl);
+		return 0;
+	}
+	
+	private Bin addBin(String binName, Field value, DataType dataType, List<Bin> bins) {
+		Bin bin = null;
+		if (value != null && binName != null) {
+			switch (value.getType()) {
+			case STRING:
+				bin = new Bin(binName, value.getValueAsString());
+				break;
+			case INTEGER:
+				bin = new Bin(binName, value.getValueAsInteger());
+				break;
+			case LONG:
+				bin = new Bin(binName, value.getValueAsLong());
+				break;
+			case BYTE_ARRAY:
+				bin = new Bin(binName, value.getValueAsByteArray());
+				break;
+			case MAP:
+				bin = new Bin(binName, Value.get(unpackField(value)));
+				break;
+			default:
+				bin = new Bin(binName, Value.get(value.getValue()));
+				break;
+			}
+		}
+		if (bin != null) {
+			bins.add(bin);
+		}
+		return bin;
+	}
+	/**
+	 * Writes a single record to the destination.
+	 *
+	 * @param record the record to write to the destination.
+	 * @throws OnRecordErrorException when a record cannot be written.
+	 */
+	private void write(Record record) throws OnRecordErrorException {
+		if (!record.has(conf.keyExpr)) {
+			throw new OnRecordErrorException(Errors.SAMPLE_01, record, "Missing key from field {}", conf.keyExpr);
+		}
+		int ttl = getTimeToLive(conf.timeToLive, record);
+		List<Bin> bins = new ArrayList<Bin>();
+		Field keyField = record.get(conf.keyExpr);
+		
+		Key key = null;
+		switch (keyField.getType()) {
+		case STRING:
+			key = new Key(conf.namespaceName, conf.setName, keyField.getValueAsString());
+			break;
+		case INTEGER:
+			key = new Key(conf.namespaceName, conf.setName, keyField.getValueAsInteger());
+			break;
+		case LONG:
+			key = new Key(conf.namespaceName, conf.setName, keyField.getValueAsLong());
+			break;
+		case BYTE_ARRAY:
+			key = new Key(conf.namespaceName, conf.setName, keyField.getValueAsByteArray());
+			break;
+		default:
+			key = new Key(conf.namespaceName, conf.setName, Value.get(keyField.getValue()));
+			break;
+		}
+
+		if (conf.allFields) {
+			for (String path : record.getEscapedFieldPaths()) {
+				String binName = null;
+				Field field = record.get(path);
+				LOG.info("path: {}, value: {}", path, field.getValue());
+				if (path.matches(".*/[a-zA-Z0-9-_]+")) {
+					binName = path.substring(path.lastIndexOf("/")+1);
+					if (binName.length() > MAX_BIN_NAME) {
+						LOG.error("Ignoring bin '{}': Name too long ({} max)", binName, MAX_BIN_NAME);
+					}
+				}
+				else {
+					LOG.error("Path '{}' contains no valid bin name after a final / character", path);
+				}
+				if (binName != null && field != null && binName != conf.keyExpr) {
+					if (conf.binNameTooLongAction == BinNameTooLongAction.TRUNCATE && binName.length() > MAX_BIN_NAME) {
+						binName = binName.substring(0, MAX_BIN_NAME);
+					}
+					addBin(binName, field, DataType.DEFAULT, bins);
+				}
+			}
+		}
+		else {
+			for(AerospikeFieldMappingConfig parameters : conf.aerospikeFieldMapping) {
+				Field value = null;
+				if (record.has(parameters.valueExpr)) {
+					value = record.get(parameters.valueExpr);
+				}
+				addBin(parameters.binName, value, parameters.dataType, bins);
+			}
+		}
+		if (bins.size() > 0) {
+			WritePolicy writePolicy = new WritePolicy();
+			writePolicy.sendKey = conf.storePK;
+			writePolicy.expiration = ttl;
+			writePolicy.recordExistsAction = conf.recordExistsAction.getAction();
+			client.put(writePolicy, null, key, bins.toArray(new Bin[bins.size()]));
+		}
+		else {
+			throw new OnRecordErrorException(Errors.SAMPLE_01, record, "No bins to write, check log for details.");
+		}
+	}
+	
+	private Object unpackField(Field field){
+		switch (field.getType()) {
+		case STRING:
+			return field.getValueAsString();
+		case INTEGER:
+			return field.getValueAsInteger();
+		case LONG:
+			return field.getValueAsLong();
+		case BYTE_ARRAY:
+			return field.getValueAsByteArray();
+		case MAP:
+			@SuppressWarnings("unchecked") Map<String, ?> fieldMap = (Map<String, ?>) field.getValue();
+			Map<String, Object> map = new LinkedHashMap<>();
+			for (Map.Entry<String, ?> entry : fieldMap.entrySet()) {
+				if(entry.getValue() instanceof Field)
+					map.put(entry.getKey(), unpackField((Field)entry.getValue()));
+				else map.put(entry.getKey(), entry.getValue());
+			}
+			return map;
+		case LIST:
+			List<?> fList = (List<?>) field.getValue();
+			List<Object> list = new ArrayList<>(fList.size());
+			for (Object element : fList) {
+				if(element instanceof Field) list.add(unpackField((Field)element));
+				else list.add(element);
+			}
+			return list;			
+		default: return field.getValue();
+		}
+	}
+}
---
+++ aerospike-lib/src/main/java/com/streamsets/pipeline/stage/destination/aerospike/AerospikeTargetConfig.java
@@ -0,0 +1,228 @@
+/**
+ * Copyright 2016 StreamSets Inc.
+ *
+ * Licensed under the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.streamsets.pipeline.stage.destination.aerospike;
+
+import java.util.List;
+
+import com.streamsets.pipeline.stage.destination.display.BinNameTooLongAction;
+import com.streamsets.pipeline.stage.destination.display.BinNameTooLongActionChooserValues;
+import com.streamsets.pipeline.stage.destination.display.RecordExistsAction;
+import com.streamsets.pipeline.stage.destination.display.RecordExistsActionChooserValues;
+import com.streamsets.pipeline.api.ConfigDef;
+import com.streamsets.pipeline.api.FieldSelectorModel;
+import com.streamsets.pipeline.api.ListBeanModel;
+import com.streamsets.pipeline.api.ValueChooserModel;
+
+public class AerospikeTargetConfig {
+
+  public static final String AEROSPIKE_TARGET_CONFIG_PREFIX = "AerospikeTargetConfig.";
+
+  @ConfigDef(
+	      required = true,
+	      type = ConfigDef.Type.STRING,
+	      defaultValue = "127.0.0.1",
+	      label = "Seed Host",
+	      description = "Seed node to connect to Aerospike",
+	      displayPosition = 10,
+	      group = "DATABASE"
+	  )
+  public String seedHost = "127.0.0.1";
+
+  @ConfigDef(
+		  required = true,
+		  type = ConfigDef.Type.NUMBER,
+		  defaultValue = "3000",
+		  label = "Seed Port",
+		  description = "Port on which to connect to Aerospike (default: 3000)",
+		  displayPosition = 20,
+		  min = 1024,
+		  group = "DATABASE"
+		  )
+  public int port = 3000;
+
+  /** {@inheritDoc} */
+  @ConfigDef(
+      type = ConfigDef.Type.NUMBER,
+      label = "Connection Timeout (sec)",
+      defaultValue = "1000",
+      required = true,
+      min = 1,
+      displayPosition = 30,
+      group = "DATABASE"
+  )
+  public int connectionTimeout = 1000;
+
+  @ConfigDef(
+	      type = ConfigDef.Type.MODEL,
+	      label = "Record Exists Action",
+	      defaultValue = "UPDATE",
+	      required = true,
+	      displayPosition = 10,
+	      group = "ADVANCED"
+	  )
+  @ValueChooserModel(RecordExistsActionChooserValues.class)
+  public RecordExistsAction recordExistsAction = RecordExistsAction.UPDATE;
+
+  @ConfigDef(
+	      required = true,
+	      type = ConfigDef.Type.MODEL,
+	      defaultValue = "/",
+	      label = "Primary Key",
+	      description = "Field to use for the primary key",
+	      group = "FIELD_MAPPING",
+	      displayPosition = 40)
+  @FieldSelectorModel(singleValued = true)
+  public String keyExpr;
+
+  @ConfigDef(
+	      required = true,
+	      type = ConfigDef.Type.MODEL,
+	      defaultValue = "0",
+	      label = "Time To Live (sec)",
+	      description = "Field to use for the time to live, -1 is infinite, 0 gets value from cluster, positive number for TTL in seconds.",
+	      group = "FIELD_MAPPING",
+	      displayPosition = 45)
+  @FieldSelectorModel(singleValued = true)
+  public String timeToLive;
+
+  @ConfigDef(
+	      required = true,
+	      type = ConfigDef.Type.BOOLEAN,
+	      label = "Store non-hashed key",
+	      description = "Whether the primary key should be written to the database, or just the digest",
+	      defaultValue = "false",
+	      displayPosition = 45,
+	      group = "FIELD_MAPPING"
+	  )
+  public boolean storePK = true;
+
+  @ConfigDef(
+	      required = true,
+	      type = ConfigDef.Type.STRING,
+	      defaultValue = "test",
+	      label = "Namespace",
+	      description = "Namespace which will house the set where the data will be written",
+	      displayPosition = 50,
+	      group = "FIELD_MAPPING"
+	  )
+  public String namespaceName = "test";
+
+  @ConfigDef(
+	      required = true,
+	      type = ConfigDef.Type.STRING,
+	      defaultValue = "testSet",
+	      label = "Set",
+	      description = "Name of the set to which the data will be written",
+	      displayPosition = 60,
+	      group = "FIELD_MAPPING"
+	  )
+  public String setName = "testSet";
+
+  
+  @ConfigDef(
+	      required = true,
+	      type = ConfigDef.Type.BOOLEAN,
+	      label = "Write all fields",
+	      description = "Whether to write the data in batches as key-value pairs or to publish the data as messages",
+	      defaultValue = "true",
+	      displayPosition = 70,
+	      group = "FIELD_MAPPING"
+	  )
+  public boolean allFields = true;
+
+  @ConfigDef(
+	      type = ConfigDef.Type.MODEL,
+	      label = "Bin Name Too Long Action",
+	      defaultValue = "TRUNCATE",
+	      required = true,
+	      displayPosition = 90,
+	      group = "FIELD_MAPPING",
+	      dependsOn = "allFields",
+	      triggeredByValue = {"true"}
+	  )
+  @ValueChooserModel(BinNameTooLongActionChooserValues.class)
+  public BinNameTooLongAction binNameTooLongAction = BinNameTooLongAction.TRUNCATE;
+
+  
+  @ConfigDef(
+      required = false,
+      type = ConfigDef.Type.MODEL,
+      defaultValue = "",
+      label = "Fields",
+      description = "Key names, their values and storage type",
+      displayPosition = 80,
+      group = "FIELD_MAPPING",
+      dependsOn = "allFields",
+      triggeredByValue = {"false"}
+  )
+  
+  @ListBeanModel
+  public List<AerospikeFieldMappingConfig> aerospikeFieldMapping;
+  
+//  
+// 	@ConfigDef(
+// 		required = true,
+// 		type = ConfigDef.Type.BOOLEAN,
+// 		label = "Use Credentials",
+// 		defaultValue = "false",
+// 		displayPosition = 40,
+// 		group = "DATABASE"
+// 	)
+// 	public boolean useCredentials = false;
+//
+
+  @ConfigDef(
+      required = true,
+      type = ConfigDef.Type.MODEL,
+      label = "Authentication Provider",
+      defaultValue = "NONE",
+      displayPosition = 30,
+      group = "DATABASE"
+  )
+  @ValueChooserModel(AuthenticatorClassChooserValues.class)
+  public AuthProviderOption authProviderOption = AuthProviderOption.NONE;
+
+  
+ 	@ConfigDef(
+ 		required = true,
+ 		type = ConfigDef.Type.STRING,
+ 		label = "Username",
+ 		defaultValue = "",
+ 		displayPosition = 10,
+ 		group = "CREDENTIALS",
+ 		dependsOn = "authProviderOption",
+ 		triggeredByValue = {"PLAINTEXT", "DSE_PLAINTEXT"}
+ 	 )
+ 	public String username;
+
+ 	@ConfigDef(
+ 		required = true,
+ 		type = ConfigDef.Type.STRING,
+ 		label = "Password",
+ 		defaultValue = "",
+ 		displayPosition = 20,
+ 		group = "CREDENTIALS",
+ 		dependsOn = "authProviderOption",
+ 		triggeredByValue = {"PLAINTEXT", "DSE_PLAINTEXT"}
+ 	)
+ 	public String password;
+
+}
